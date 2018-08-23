945dff54344c9650bfa3b2544cd6d24cf4bdbfb7
hbdgr <hbdgr@protonmail.com>
2018-08-23 09:32:07 +0200

    password -> passwd

--- src/crypto.rs
+++ src/crypto.rs
@@ -8,6 +8,7 @@ use bincode;
 use error::Error;
 use utils::{ to_hex_string };
 
+
 pub fn pubkey_to_str(pubkey: &sign::PublicKey) -> String {
 	let mut hex_string = String::new();
 
@@ -23,7 +24,7 @@ pub struct PersonalKeys {
 	// singing public key
 	pub pk: sign::PublicKey,
 
-	// password_hash
+	// password hash
 	pwh: pwhash::HashedPassword,
 
 	// secret key derive salt
@@ -46,17 +47,18 @@ impl fmt::Display for PersonalKeys {
 
 impl PersonalKeys {
 	pub fn new(password: &str) -> PersonalKeys {
-		let pwh = PersonalKeys::hash_password(&password);
+		let passwd = PersonalKeys::make_passwd(password);
+		let pwh = PersonalKeys::hash_password(&passwd);
 		let nonce = secretbox::gen_nonce();
 
 		let (pk, sk) = sign::gen_keypair();
-		let (encrypted_secret, salt) = Self::encrypt_sign_key(&sk, &nonce, password);
+		let (encrypted_secret, salt) = Self::encrypt_sign_key(&sk, &nonce, &passwd);
 
 		PersonalKeys { pk, pwh, salt, nonce, encrypted_secret }
 	}
 
 	pub fn sign(&self, message: &str, password: &str) -> Result<sign::Signature, Error> {
-		let sk = Self::decrypt_sign_key(self, &password)?;
+		let sk = Self::decrypt_sign_key(self, &PersonalKeys::make_passwd(&password))?;
 
 		let signature = sign::sign_detached(
 			message.as_bytes(),
@@ -73,35 +75,38 @@ impl PersonalKeys {
 		}
 	}
 
+	fn make_passwd(password: &str) -> Vec<u8> {
+		format!("c@p)*c=#e{}", password).into_bytes()
+	}
 
-	fn hash_password(password: &str) -> pwhash::HashedPassword {
-		let pwh = pwhash::pwhash(password.as_bytes(),
+	fn hash_password(passwd: &[u8]) -> pwhash::HashedPassword {
+		let pwh = pwhash::pwhash(passwd,
 		                         pwhash::OPSLIMIT_INTERACTIVE,
 		                         pwhash::MEMLIMIT_INTERACTIVE).unwrap();
 		pwh
 	}
 
-	fn verify_password(&self, password: &str) -> Result<(), Error> {
-		match pwhash::pwhash_verify(&self.pwh, password.as_bytes()) {
+	fn verify_password(&self, passwd: &[u8]) -> Result<(), Error> {
+		match pwhash::pwhash_verify(&self.pwh, passwd) {
 			true => Ok(()),
 			false => Err(Error::WrongPassword)
 		}
 	}
 
-	fn derive_key(password: &str, salt: &pwhash::Salt) -> secretbox::Key {
+	fn derive_key(passwd: &[u8], salt: &pwhash::Salt) -> secretbox::Key {
 		let mut k = secretbox::Key([0; secretbox::KEYBYTES]);
 		{
 			let secretbox::Key(ref mut kb) = k;
-			pwhash::derive_key(kb, password.as_bytes(), &salt,
+			pwhash::derive_key(kb, passwd, &salt,
 			                   pwhash::OPSLIMIT_INTERACTIVE,
 			                   pwhash::MEMLIMIT_INTERACTIVE).unwrap();
 		}
 		k
 	}
 
-	fn encrypt_sign_key(sk: &sign::SecretKey, nonce: &secretbox::Nonce, password: &str) -> (Vec<u8>, pwhash::Salt) {
+	fn encrypt_sign_key(sk: &sign::SecretKey, nonce: &secretbox::Nonce, passwd: &[u8]) -> (Vec<u8>, pwhash::Salt) {
 		let salt = pwhash::gen_salt();
-		let encryption_key = Self::derive_key(&password, &salt);
+		let encryption_key = Self::derive_key(&passwd, &salt);
 
 		let sk_bytes = bincode::serialize(&sk).unwrap();
 		let encrypted_secret = secretbox::seal(&sk_bytes, &nonce, &encryption_key);
@@ -109,10 +114,10 @@ impl PersonalKeys {
 		(encrypted_secret, salt)
 	}
 
-	fn decrypt_sign_key(&self, password: &str) -> Result<sign::SecretKey, Error> {
-		self.verify_password(password)?;
+	fn decrypt_sign_key(&self, passwd: &[u8]) -> Result<sign::SecretKey, Error> {
+		self.verify_password(passwd)?;
 
-		let encryption_key = Self::derive_key(&password, &self.salt);
+		let encryption_key = Self::derive_key(&passwd, &self.salt);
 		let sk_bytes = secretbox::open(&self.encrypted_secret, &self.nonce, &encryption_key).unwrap();
 
 		let sk: sign::SecretKey = bincode::deserialize(&sk_bytes).unwrap();
