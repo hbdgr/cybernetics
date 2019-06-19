3f19a326266a11c255ccab4819d20229c10f4a78
hbdgr <hbdgr@protonmail.com>
2019-06-19 11:29:14 +0200

    router update

--- src/server/router.rs
+++ src/server/router.rs
@@ -5,29 +5,43 @@ use rocket::http::Method;
 use rocket_cors::{AllowedOrigins, AllowedHeaders};
 
 
-#[get("/")]
+#[options("/")]
 fn cors<'a>() -> &'a str {
 	"Hello CORS"
 }
 
-pub fn create_routes() {
+fn cors_options() -> rocket_cors::Cors {
 	let (allowed_origins, failed_origins) = AllowedOrigins::some(&["http://localhost:8080"]);
 	assert!(failed_origins.is_empty());
 
 	// You can also deserialize this
-	let options = rocket_cors::Cors {
+	rocket_cors::Cors {
 		allowed_origins: allowed_origins,
-		allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
-		allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-Type"]),
+		allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete, Method::Options]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::all(),
+		// allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
+		// allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-Type"]),
 		allow_credentials: true,
 		..Default::default()
-	};
+	}
+}
 
+pub fn create_routes() {
 	rocket::ignite()
 		.manage(connection_pool::init_pool())
+        .manage(cors_options())
 		.mount(
 			"/",
-			routes![cors]
+			rocket_cors::catch_all_options_routes()
+		)
+		.mount(
+			"/",
+			routes![
+				handler::auth_post
+			],
 		)
 		.mount(
 			"/objects",
@@ -36,15 +50,9 @@ pub fn create_routes() {
 				handler::get,
 				handler::post,
 				handler::put,
-				handler::delete
+				handler::delete,
 			],
 		)
-		.mount(
-			"/",
-			routes![
-				handler::auth_post
-			],
-		)
-		.attach(options)
+		.attach(cors_options())
 		.launch();
 }
